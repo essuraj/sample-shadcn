d65d17f5264e687695457577d4b9642b42fb6ccb
Suraj Elapully <essuraj@live.in>
2024-11-14 17:29:04 +0530

    feat: add Radix UI button component and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import { useState } from 'react'
+ import { Button } from './components/ui/button'
  
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
       
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary">
+        The Joke Tax Chronicles
+      </h1>
+      <Button>Click me</Button>
+
     </>
   )
 }
